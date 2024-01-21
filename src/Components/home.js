8416ada2473c5cca4a32ab745c8634c321030233
Durga Prasad Kakileti <durgaprasadkakileti@gmail.com>
2024-01-21 13:46:31 +0530

    optimised code

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,36 +1,40 @@
-
 import React, { useState } from 'react';
+import * as math from 'mathjs';
 
 export default function Home() { 
   const [input, setInput] = useState("");
-
-  const [condition,setCondition] = useState("false")
-
-
-
   function displayInput(value) { 
     if (value === "C") {
       
       setInput("");
     }
     else if(value === "="){
-        try{
-            setInput(eval(input).toString())
-        } catch(error){
-            setInput("INVALID")
-        }
-    } 
+      try {
+        setInput(math.evaluate(input).toString());
+      } catch (error) {
+        setInput("INVALID");
+      }
+    }
+    
     else if(value === "BS"){
+       if(input === "INVALID"){
+        setInput("")
+       }
+       else{
         setInput(previnput => previnput.slice(0,-1))
+       }
+        
     }
-    else if(input === "INVALID"){
-        setInput(value)
+    else if(input === "INVALID"){ 
+        setInput(value) 
     }
+    
     else {
         setInput(previnput => previnput + value)
     }
   }
 
+  const buttonClass = 'w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded';
   return (
     <>
       <section className="bg-[#F3EEEA]  h-screen flex flex-col  justify-center items-center">
@@ -42,29 +46,29 @@ export default function Home() {
       <input  className='flex-grow py-6 rounded-lg text-center text-[#776B5D]' type="text" value={input} />
       </div>
       <div className='flex w-[85%] gap-2 m-2 lg:m-4'>
-        <button className='w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded' onClick={()=>displayInput(7)}>𝟟</button>
-        <button className='w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded' onClick={()=>displayInput(8)}>𝟠</button>
-        <button className='w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded' onClick={()=>displayInput(9)}>𝟡</button>
-        <button className='w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded' onClick={()=>displayInput("/")}>/</button>
+        <button className={buttonClass} onClick={()=>displayInput(7)}>𝟟</button>
+        <button className={buttonClass} onClick={()=>displayInput(8)}>𝟠</button>
+        <button className={buttonClass} onClick={()=>displayInput(9)}>𝟡</button>
+        <button className={buttonClass} onClick={()=>displayInput("/")}>/</button>
         
       </div>
       <div className='flex w-[85%] gap-2 m-2 lg:m-4'>
-        <button className='w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200  py-4 rounded' onClick={()=>displayInput(4)}>𝟜</button>
-        <button className='w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded' onClick={()=>displayInput(5)}>𝟝</button>
-        <button className='w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded' onClick={()=>displayInput(6)}>𝟞</button>
-        <button className='w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded' onClick={()=>displayInput('*')}>＊</button>
+        <button className={buttonClass} onClick={()=>displayInput(4)}>𝟜</button>
+        <button className={buttonClass} onClick={()=>displayInput(5)}>𝟝</button>
+        <button className={buttonClass} onClick={()=>displayInput(6)}>𝟞</button>
+        <button className={buttonClass} onClick={()=>displayInput('*')}>＊</button>
       </div>
       <div className='flex w-[85%] gap-2 m-2 lg:m-4'>
-      <button className='w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded' onClick={()=>displayInput(1)}>𝟙</button>
-        <button className='w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded' onClick={()=>displayInput(2)}>𝟚</button>
-        <button className='w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded' onClick={()=>displayInput(3)}>𝟛</button>
-        <button className='w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded' onClick={()=>displayInput('-')}>-</button>
+      <button className={buttonClass} onClick={()=>displayInput(1)}>𝟙</button>
+        <button className={buttonClass} onClick={()=>displayInput(2)}>𝟚</button>
+        <button className={buttonClass} onClick={()=>displayInput(3)}>𝟛</button>
+        <button className={buttonClass} onClick={()=>displayInput('-')}>-</button>
       </div>
       <div className='flex w-[85%] gap-2 m-2 lg:m-4'>
-      <button className='w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded' onClick={()=>displayInput('.')}>.</button>
-        <button className='w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded' onClick={()=>displayInput(0)}>𝟘</button>
-        <button className='w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded' onClick={()=>displayInput("BS")}>←</button>
-        <button className='w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded' onClick={()=>displayInput("+")}>+</button>
+      <button className={buttonClass} onClick={()=>displayInput('.')}>.</button>
+        <button className={buttonClass} onClick={()=>displayInput(0)}>𝟘</button>
+        <button className={buttonClass} onClick={()=>displayInput("BS")}>←</button>
+        <button className={buttonClass} onClick={()=>displayInput("+")}>+</button>
       </div>
       <div className='flex w-[85%] gap-2'>
       <button className='bg-[#776B5D] text-white py-4 rounded w-[50%] hover:shadow-lg' onClick={() => displayInput("C")}>𝔸ℂ</button>
