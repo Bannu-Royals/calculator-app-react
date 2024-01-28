6f8408b82c9444384866e1698ac8b30c6fa38413
Durga Prasad Kakileti <durgaprasadkakileti@gmail.com>
2024-01-28 10:25:49 +0530

    new feature

--- src/Components/home.js
+++ src/Components/home.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import * as math from 'mathjs';
 
 export default function Home() { 
-  const [input, setInput] = useState("");
+  const [input , setInput] = useState("");
   function displayInput(value) { 
     if (value === "C") {
       setInput("");
@@ -21,27 +21,25 @@ export default function Home() {
        }
        else{
         setInput(previnput => previnput.slice(0,-1))
-       }
-        
+       }    
     }
     else if(input === "INVALID"){ 
         setInput(value) 
     }
-    
     else {
-        setInput(previnput => previnput + value)
+        setInput((previnput) => previnput + value)
     }
   }
   const buttonClass = 'w-[30%] bg-[#EBE3D5] hover:bg-[#776B5D] shadow-lg duration-200 py-4 rounded';
   return (
     <>
-      <section className="bg-[#F3EEEA] h-screen flex flex-col  justify-center items-center">
-       <section className='bg-[#B0A695] flex flex-col rounded-lg justify-center items-center lg:h-[85%] lg:w-[35%] w-[85%] h-[75%] md:w-[45%] text-2xl shadow-lg xl:w-[25%]'>
+      <section  className="bg-[#F3EEEA] h-screen flex flex-col  justify-center items-center">
+       <section draggable="true" className='bg-[#B0A695] flex flex-col rounded-lg justify-center items-center lg:h-[85%] lg:w-[35%] w-[85%] h-[75%] md:w-[45%] text-2xl shadow-lg xl:w-[25%]'>
         <div>
             <p className='text-2xl lg:text-3xl text-[#342a1f]'>ᑕᗩᒪᑕᑌᒪᗩTOᖇ</p>
         </div>
        <div className='flex w-[85%] m-4'>
-      <input  className='flex-grow py-6 bg-[#776B5D] rounded-lg text-center text-[#EBE3D5]' type="text" value={input} />
+      <input  className='flex-grow py-6 bg-[#776B5D] cursor-none rounded-lg text-center text-[#EBE3D5]' type="text" value={input} />
       </div>
       <div className='flex w-[85%] gap-2 m-2 lg:m-4'>
         <button className={buttonClass} onClick={()=>displayInput(7)}>𝟟</button>
